ae1cfe550a012e20fcdb94a8e22a895ba7f739f7
zhugezifang <xiaoqiu2017wy@163.com>
2025-01-03 07:49:16 +0000

    feat:copy feature

--- app/[lang]/(default)/_components/copy/index.tsx
+++ app/[lang]/(default)/_components/copy/index.tsx
@@ -1,32 +1,28 @@
 'use client';
 import Script from 'next/script';
-//import React, { useState } from 'react';
 
 export default function ({ code,copy,daima }: { code: string;copy:string;daima:string}) {
 
+  const copyText = async () => {
+    try {
+      await navigator.clipboard.writeText(code);
+      alert("copy success!");
+    } catch (err) {
+      alert("copy failed!");
+    }
+  };
+
   return (
+    <>
+    
     <div className="container max-w-2xl mx-auto p-4">
-      <Script strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            const copyButton = document.getElementById('copyButton');
-            copyButton.addEventListener('click', function() {
-                navigator.clipboard.writeText(document.getElementById('code').value)
-                    .then(() => {
-                        alert('Code copied to clipboard!');
-                    })
-                    .catch(err => {
-                        console.error('Failed to copy code: ', err);
-                    });
-            });
-            `,
-          }}
-      />
+      
           <h2 className="text-2xl font-bold mb-4">{daima}</h2>
           <div className="bg-gray-100 p-4 rounded-lg flex items-center justify-between">
-              <code id="code" className="text-lg font-medium">{code}</code>
-              <button id="copy-btn" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">{copy}</button>
+              <span id="code" className="text-lg font-medium">{code}</span>
+              <button id="copy-btn" onClick={copyText} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">{copy}</button>
           </div>
     </div>
+    </>
   );
 }
