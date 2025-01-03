6403b1519f933951dab097666bd969c93186e749
zhugezifang <xiaoqiu2017wy@163.com>
2025-01-03 10:15:07 +0000

    feat:code

--- app/[lang]/(default)/_components/copy/index.tsx
+++ app/[lang]/(default)/_components/copy/index.tsx
@@ -17,7 +17,6 @@ export default function ({ code,copy,daima }: { code: string;copy:string;daima:s
     
     <div className="container max-w-2xl mx-auto p-4">
       
-          <h2 className="text-2xl font-bold mb-4">{daima}</h2>
           <div className="bg-gray-100 p-4 rounded-lg flex items-center justify-between">
               <span id="code" className="text-lg font-medium">{code}</span>
               <button id="copy-btn" onClick={copyText} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">{copy}</button>
