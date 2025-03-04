9c9f5f4ca3aa682425b9decbc9601fd88969a17b
zhugezifang <xiaoqiu2017wy@163.com>
2025-03-04 14:36:54 +0000

    feat:zh en

--- app/[lang]/(default)/_components/tab/index.tsx
+++ app/[lang]/(default)/_components/tab/index.tsx
@@ -29,7 +29,142 @@ export default function ({
   const RadissonTions: Code[] = dict.Radisson.data;
 
   return (
+    <>
+    <div className="my-10">
+        <h2 className="text-3xl text-center font-bold mb-10">{dict.title}</h2>
+        <div className="max-w-[1500px] mx-auto grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 
+        gap-4 sm:gap-6 lg:gap-6
+        justify-items-center">
+          <a href={`/${lang}/${dict.Hilton.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/hilton.png" alt="Hilton Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <symbol id="ai:ic:twotone-discount">
+                    <path fill="currentColor" d="M19 9.79L11.21 2H5v6.21L12.79 16zM7.25 5.5a1.25 1.25 0 1 1 0-2.5a1.25 1.25 0 0 1 0 2.5" opacity=".3"></path>
+                    <path fill="currentColor" d="M12.79 21L3 11.21v2c0 .53.21 1.04.59 1.41l7.79 7.79c.78.78 2.05.78 2.83 0l6.21-6.21c.78-.78.78-2.05 0-2.83z"></path>
+                    <path fill="currentColor" d="M11.38 17.41c.39.39.9.59 1.41.59s1.02-.2 1.41-.59l6.21-6.21c.78-.78.78-2.05 0-2.83L12.62.58C12.25.21 11.74 0 11.21 0H5C3.9 0 3 .9 3 2v6.21c0 .53.21 1.04.59 1.41zM5 2h6.21L19 9.79L12.79 16L5 8.21z"></path>
+                    <circle cx="7.25" cy="4.25" r="1.25" fill="currentColor"></circle>
+                  </symbol>
+                  <use></use>
+                </svg>
+                <span>25% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Hilton.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Marriott.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/marriott.png" alt="Marriott Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>22% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Marriott.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Hyatt.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/hyatt.png" alt="Hyatt Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>16% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Hyatt.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.IHG.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/ihg.png" alt="IHG Hotels Group logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>20% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.IHG.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Western.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/best-western.png" alt="Best Western Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>23% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Western.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Choice.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/choice-hotels.png" alt="Choice Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>19% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Choice.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Omni.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/omni.jpg" alt="Omni Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>20% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Omni.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Wyndham.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/wyndham.png" alt="Wyndham Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>23% off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Wyndham.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+          <a href={`/${lang}/${dict.Radisson.url}`} className="block w-52 min-w-[128px] max-w-sm bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
+            <div className="relative h-36">
+              <img src="/images/radisson.png" alt="Radisson Hotels logo" className="w-full h-full object-cover"/>
+              <div className="absolute top-2 left-2 border-2 border-gray-300 bg-white text-gray-800 px-4 py-1 text-sm flex items-center rounded-full">
+                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-emerald-600" data-icon="ic:twotone-discount">
+                  <use></use>
+                </svg>
+                <span>16%off</span></div>
+            </div>
+            <div className="p-2">
+              <h3 className="font-semibold text-base text-gray-800 mb-1 truncate">{dict.Radisson.t0}</h3>
+              <span className="inline-block bg-gray-200 text-gray-400 px-4 py-1 text-xs rounded-full whitespace-nowrap">{dict.brand.name} &gt;</span></div>
+          </a>
+        </div>
+    </div>
+
     <div className="mx-auto mt-8 mb-0">
+
+      
+
      <div className="relative isolate overflow-hidden px-6 py-6  sm:rounded-3xl sm:px-24 xl:pt-4 flex justify-between items-center">
       <h2 className="text-2xl font-bold">{dict.Hilton.title}</h2>
       <a href={`/${lang}/${dict.Hilton.url}`} className="hover:text-blue-700">{dict.Hilton.more}</a>
@@ -591,7 +726,7 @@ export default function ({
           </div>
         </div>
     </div>
-
+    </>
     
   );
 }
