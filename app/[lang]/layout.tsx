c8b5d00c28d11c1362293c4385fedc75fe7d7507
诸葛子房 <xiaoqiu2017wy@163.com>
2026-05-13 22:49:34 +0800

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
     <html lang={lang}>
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-adsense-account" content="ca-pub-4158779929727637">
+        <meta name="google-adsense-account" content="ca-pub-4158779929727637"/>
         {/* Bug 修复: 将 crossorigin 改为 crossOrigin */}
       </head>
       <body className={inter.className}>
